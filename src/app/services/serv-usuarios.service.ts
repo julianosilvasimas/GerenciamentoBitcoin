b8fbb971d3ca2f54e610a083f000abd66a2b47aa
VitorHeser <54639202+VitorHeser@users.noreply.github.com>
2021-05-23 13:57:02 -0300

    Versão 01 - Clientes e Usuários

--- src/app/services/serv-usuarios.service.ts
+++ src/app/services/serv-usuarios.service.ts
@@ -13,9 +13,17 @@ export class ServUsuariosService {
   constructor(private http: HttpClient){}
   httpOptions = {headers: new HttpHeaders().set('Authorization',sessionStorage.getItem('token'))}
 
+  getEscritorios(): Observable<any[]>{
+    return this.http.get(`${API_CONFIG}/usuarios/escritorios`) 
+    .pipe(map((res : any[]) => res, catchError(ErrorHandler.handleError)))
+  }
   
   getUsers(): Observable<any[]>{
     return this.http.get(`${API_CONFIG}/usuarios`) 
+    .pipe(map((res : any[]) => res, catchError(ErrorHandler.handleError)))
+  }
+  getUser(id): Observable<any[]>{
+    return this.http.get(`${API_CONFIG}/usuarios/${id}`) 
     .pipe(map((res : any[]) => res, catchError(ErrorHandler.handleError)))
   }
   getUserByEmail(email): Observable<any[]>{
@@ -27,11 +35,24 @@ export class ServUsuariosService {
     .pipe(map((res : any[]) => res, catchError(ErrorHandler.handleError)))
   }
   putUser(user): Observable<any[]>{
-    return this.http.put(`${API_CONFIG}/usuarios/pu2/${user.id}`,user) 
+    return this.http.put(`${API_CONFIG}/usuarios/${user.id}`,user) 
     .pipe(map((res : any[]) => res, catchError(ErrorHandler.handleError)))
   }
   deleteUser(user): Observable<any[]>{
     return this.http.delete(`${API_CONFIG}/usuarios/${user.id}`) 
+    .pipe(map((res : any[]) => res, catchError(ErrorHandler.handleError)))
+  }
+
+
+
+  attSenha(user): Observable<any[]>{
+    return this.http.put(`${API_CONFIG}/usuarios/attsenha/${user.id}`,user) 
+    .pipe(map((res : any[]) => res, catchError(ErrorHandler.handleError)))
+  }
+  atualizarFoto(id,foto): Observable<any[]>{
+    const formData = new FormData();
+    formData.append('file',foto);
+    return this.http.post(`${API_CONFIG}/usuarios/picture/${id}`,formData) 
     .pipe(map((res : any[]) => res, catchError(ErrorHandler.handleError)))
   }
 }
