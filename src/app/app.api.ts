facae5471102ab5beca79d695e2b2c7a6aeab716
Juliano da Silva Simas <juliano.developer01@gmail.com>
2021-06-08 05:06:59 -0300

    + Segurança

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -12,16 +12,6 @@ export const API_CONFIG =  'http://200.98.171.178:7777/bitinvestapi' //TESTE AND
 //export const API_CONFIG =  'http://localhost:7777'
 
 
-
-//manager
-//login: SabeEstoque
-//senha: Sabe2012
-
-//FTP
-//https://mydevelopment.6te.net/ftp/
-//login: mydevelopment.6te.net
-//senha: 20JVsystems21
-
 export const API_BLOCK = 3.974e+9  // 4 dias de bloqueio 3.456e+8   // 15 dias de bloqueio 1.296e+9
 
 export const API_BLOCK_REC = 3.974e+9  //Acima de 31 dias 2.765e+9             Acima de 45 dias //3.97ng
